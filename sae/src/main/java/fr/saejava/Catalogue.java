2442e6d0f3295cda79ca2b26bf1d6118e3692023
Irvine YANG <irvine.yang@etu.univ-orleans.fr>
2025-05-19 12:20:49 +0200

    création class Catalogue

---
+++ sae/src/main/java/fr/saejava/Catalogue.java
@@ -0,0 +1,21 @@
+package fr.saejava;
+
+import java.util.List;
+
+public class Catalogue {
+    List<Utilisateur> utilisateur;
+    List<Magasin> magasin;
+
+    public Catalogue(List<Utilisateur> utilisateur, List<Magasin> magasin) {
+        this.utilisateur = utilisateur;
+        this.magasin = magasin;
+    }
+
+    public List<Utilisateur> getUtilisateur() {
+        return utilisateur;
+    }
+
+    public List<Magasin> getMagasin() {
+        return magasin;
+    }
+}
