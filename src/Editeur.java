4394cd2544023ffd7edd8109a58493b6e519fba9
Alex124816 <alexandrelaurin45550@gmail.com>
2025-05-19 10:58:19 +0200

    o

---
+++ src/Editeur.java
@@ -0,0 +1,28 @@
+public class Editeur {
+    private int idEdit;
+    private String nomEdit;
+
+    public Editeur(int idEdit, String nomEdit) {
+        this.idEdit = idEdit;
+        this.nomEdit = nomEdit;
+    }
+
+    public int getIdEdit() {
+        return idEdit;
+    }
+
+    public void setIdEdit(int idEdit) {
+        this.idEdit = idEdit;
+    }
+
+    public String getNomEdit() {
+        return nomEdit;
+    }
+
+    public void setNomEdit(String nomEdit) {
+        this.nomEdit = nomEdit;
+    }
+
+    
+    
+}
