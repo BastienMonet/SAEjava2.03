389fabe19895f00cc42e90ffd866c0222a01dbd7
BastienMonet <bastien.monet@etu.univ-orleans.fr>
2025-05-20 17:05:36 +0200

    ok on repart sur des bonne bases

--- sae/src/main/java/fr/saejava/Vendeur.java
+++ sae/src/main/java/fr/saejava/Vendeur.java
@@ -2,8 +2,8 @@ package fr.saejava;
 
 public class Vendeur extends Utilisateur {
     
-    public Vendeur(int idUtil, String nomUtil, String prenomUtil, String pwd){
-        super(idUtil, nomUtil, prenomUtil, pwd);
+    public Vendeur(int idUtil, String nomUtil, String prenomUtil, String pwd, Catalogue cat){
+        super(idUtil, nomUtil, prenomUtil, pwd, cat);
     }
 
 
