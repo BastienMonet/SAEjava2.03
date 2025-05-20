eabf227ce62df8730fb82b4187b9e90032c71480
Irvine YANG <irvineyang45@gmail.com>
2025-05-20 17:00:47 +0000

    ajout méthode "transfertLIvre" de Catalogue

--- sae/src/main/java/fr/saejava/Catalogue.java
+++ sae/src/main/java/fr/saejava/Catalogue.java
@@ -1,6 +1,7 @@
 package fr.saejava;
 
 import java.util.List;
+import java.util.Map;
 
 public class Catalogue {
     List<Utilisateur> utilisateur;
@@ -21,5 +22,12 @@ public class Catalogue {
 
     public void ajouterMagasin(Magasin mag){
         this.magasin.add(mag);
+    }
+
+    public void transfertLivre(Magasin mag1, Magasin mag2, Livre livre){
+        Map<Livre, Integer> liMag1 = mag1.getLivres();
+        Map<Livre, Integer> liMag2 = mag2.getLivres();
+        liMag1.put(livre, liMag1.get(livre) - 1);
+        liMag2.put(livre, liMag2.get(livre) + 1);
     }
 }
