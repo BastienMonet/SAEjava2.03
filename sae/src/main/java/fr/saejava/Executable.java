cb3a71fdf03f30239090e6f34964770fac32f339
BastienMonet <BastienMonet@github.com>
2025-05-22 12:26:34 +0200

    genre vraiment

--- sae/src/main/java/fr/saejava/Executable.java
+++ sae/src/main/java/fr/saejava/Executable.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Executable {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
         ConnexionMySQL co = new ConnexionMySQL();
-        // co.connecter(null, "DBmonet", "monet", "monet");
+        co.connecter(null, "DBmonet", "monet", "monet");
        
         
         
