6593d71f1374bfe42f1c8c32069b00c4f9d77ff8
BastienMonet <BastienMonet@github.com>
2025-05-22 12:09:42 +0200

    c la merde

--- sae/src/main/java/fr/saejava/Executable.java
+++ sae/src/main/java/fr/saejava/Executable.java
@@ -4,13 +4,10 @@ import java.sql.SQLException;
 
 public class Executable {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
-        try {
-            ConnexionMySQL co = new ConnexionMySQL();
-        } catch (ClassNotFoundException e){
-             System.err.println("Driver non trouve");
-        }
+        ConnexionMySQL co = new ConnexionMySQL();
+        // co.connecter(null, "DBmonet", "monet", "monet");
        
-        //  co.connecter(null, "DBmonet", "monet", "monet");
+        
         
          
     }
