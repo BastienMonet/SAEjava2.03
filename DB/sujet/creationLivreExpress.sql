630444881e721e73d39823e55d4342cdb17e70b7
BastienMonet <bastien.monet@etu.univ-orleans.fr>
2025-05-23 21:25:13 +0200

    retire monnaie pour avoir acces au jeu de donnée

--- DB/sujet/creationLivreExpress.sql
+++ DB/sujet/creationLivreExpress.sql
@@ -45,8 +45,8 @@ CREATE TABLE CLIENT(
   iduse      int NOT NULL,
   adressecli varchar(100),
   codepostal varchar(5),
-  villecli   varchar(100),
-  monnaie decimal(6,2)
+  villecli   varchar(100)
+  -- monnaie decimal(6,2)
 );
 
 CREATE TABLE ADMINISTRATEUR(
