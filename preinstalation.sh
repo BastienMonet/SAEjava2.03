9fcaf82ff3e2ca09f86438ffdb7414d49fc2396f
BastienMonet <BastienMonet@github.com>
2025-06-20 12:08:07 +0200

    fix preinstallation

--- preinstalation.sh
+++ preinstalation.sh
@@ -1,4 +1,19 @@
 
+#!/bin/bash
+
+# Demander les paramètres à l'utilisateur
+read -p "Hôte de la base de données (servinfo-maria par défaut sinon tappé localhost) : " DB_HOST
+DB_HOST=${DB_HOST:-servinfo-maria}
+
+read -p "Nom d'utilisateur : " DB_USER
+read -s -p "Mot de passe : " DB_PASS
+echo ""
+read -p "Nom de la base de données : " DB_NAME
+
+mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" <<EOF
+source DB/sujet/destructionLivreExpress.sql
+source DB/sujet/creationLivreExpress.sql
+EOF
 
 # pre installation
 
